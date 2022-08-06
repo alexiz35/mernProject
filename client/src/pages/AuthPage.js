0b14fb857af1ecfb769a2c2bc224d03dacc1c0fa
Aleksandr <aleksiz1977@gmail.com>
2022-08-06 23:14:54 +0300

    create rotating Auth form

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -2,21 +2,21 @@ import React, {useContext, useEffect, useState} from "react";
 import {useNavigate} from "react-router";
 import {useHttp} from "../hooks/http.hook";
 import {AuthContext} from "../context/AuthContext";
+import styled from './AuthPage.module.css'
+import Electro from "../components/Electro";
+
 
 export const AuthPage = () => {
     const navigate = useNavigate()
     const auth = useContext(AuthContext)
     const {loading, request, error, clearError} = useHttp()
+
     const [form, setForm] = useState({
         email: '', password: ''
     })
+//----------------------------------------------------------------------
 
-    useEffect(() => {
-        error && alert(error)
-        clearError()
-
-    }, [error, clearError])
-
+//----------------------------------------------------------------------
 
     const changeHandler = event => {
         setForm({...form, [event.target.name]: event.target.value})
@@ -41,53 +41,179 @@ export const AuthPage = () => {
         }
     }
 
+    useEffect(() => {
+        error && alert(error)
+        clearError()
+
+    }, [error, clearError])
+
+    /*useEffect(()=>{
+        window.location.reload()
+    },[auth])*/
+
+
     return (
-        <div className="row d-flex justify-content-center" style={{marginTop: '100px'}}>
-            <div className="col-7 ">
-                <h1>Auth page</h1>
-                <div className="form-floating mb-3">
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="email"
-                        name="email"
-                        placeholder="name@example.com"
-                        value={form.email}
-                        onChange={changeHandler}
-                    />
-                    <label htmlFor="email">Email address</label>
-                </div>
-                <div className="form-floating">
-                    <input
-                        type="password"
-                        className="form-control"
-                        id="password"
-                        name="password"
-                        placeholder="Password"
-                        value={form.password}
-                        onChange={changeHandler}
-                    />
-                    <label htmlFor="password">Password</label>
-                </div>
-                <div className="mt-4" >
-                    <button
-                        className="btn-dark btn me-3 col-12"
-                        /*style={{marginRight: 10}}*/
-                        disabled={loading}
-                        onClick={loginHandler}
-                    >
-                        Login
-                    </button>
-                    <button
-                        className={"btn-dark btn me-3 col-12 mt-3"}
-                        onClick={registerHandler}
-                        disabled={loading}
-                    >
-                        Registration
-                    </button>
+        <>
+            <div>
+            {/*<canvas id="canvas" className={styled.canvas}/>
+            <Electro/>*/}
+            <div className={styled.section}>
+                <div className="container">
+                    <div className={`row ${styled.fullHeight}  justify-content-center`}>
+                        <div className="col-12 text-center align-self-center py-5 ">
+                            <div className={styled.section + " pb-5 pt-5 pt-sm-2 text-center"}>
+                                <h6 className="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
+                                <input className={styled.checkbox} type="checkbox" id="reg-log" name="reg-log"/>
+                                <label htmlFor="reg-log"/>
+                                <div className={styled.card3dWrap + " mx-auto"}>
+                                    <div className={styled.card3dWrapper}>
+
+                                        <div className={styled.cardFront}>
+                                            <div className={styled.centerWrap}>
+                                                <div className={styled.section + " text-center"}>
+                                                    <h4 className="mb-4 pb-3">Log In</h4>
+
+                                                    <div className={styled.formGroup}>
+                                                        <input
+                                                            type="email"
+                                                            name="email"
+                                                            id="email"
+                                                            className={styled.formStyle}
+                                                            placeholder="Your Email"
+                                                            value={form.email}
+                                                            onChange={changeHandler}
+                                                            autoComplete="off"
+                                                        />
+                                                        <i className={styled.inputIcon + " bi bi-envelope-fill"}/>
+                                                    </div>
+                                                    <div className={styled.formGroup + " mt-2"}>
+                                                        <input
+                                                            type="password"
+                                                            name="password"
+                                                            className={styled.formStyle}
+                                                            placeholder="Your Password"
+                                                            id="password"
+                                                            value={form.password}
+                                                            onChange={changeHandler}
+                                                            autoComplete="off"
+                                                        />
+                                                        <i className={styled.inputIcon + " bi bi-key"}/>
+                                                    </div>
+                                                    <button
+                                                        className={styled.btn + " mt-4"}
+                                                        disabled={loading}
+                                                        onClick={loginHandler}
+                                                    >
+                                                        Login
+                                                    </button>
+                                                    {/*<a href="#" className="btn mt-4">submit</a>*/}
+                                                    <p className="mb-0 mt-4 text-center"><a href="#0"
+                                                                                            className={styled.link}>Forgot
+                                                        your password?</a></p>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className={styled.cardBack}>
+                                            <div className={styled.centerWrap}>
+                                                <div className={styled.section + " text-center"}>
+                                                    <h4 className="mb-4 pb-3">Sign Up</h4>
+                                                    {/*<div className="form-group">
+                                                    <input type="text" name="logname" className="form-style"
+                                                           placeholder="Your Full Name" id="logname" autoComplete="off"/>
+                                                        <i className="input-icon uil uil-user"/>
+                                                </div>*/}
+                                                    <div className={styled.formGroup + " mt-2"}>
+                                                        <input
+                                                            type="email"
+                                                            name="email"
+                                                            className={styled.formStyle}
+                                                            placeholder="Your Email"
+                                                            id="email"
+                                                            value={form.email}
+                                                            onChange={changeHandler}
+                                                            autoComplete="off"
+                                                        />
+                                                        <i className={styled.inputIcon + " bi bi-envelope-fill"}/>
+                                                    </div>
+                                                    <div className={styled.formGroup + " mt-2"}>
+                                                        <input
+                                                            type="password"
+                                                            name="password"
+                                                            className={styled.formStyle}
+                                                            placeholder="Your Password"
+                                                            id="password"
+                                                            value={form.password}
+                                                            onChange={changeHandler}
+                                                            autoComplete="off"
+                                                        />
+                                                        <i className={styled.inputIcon + " bi bi-key"}/>
+                                                    </div>
+                                                    <button
+                                                        className={styled.btn + " mt-4"}
+                                                        disabled={loading}
+                                                        onClick={registerHandler}
+                                                    >
+                                                        Sign Up
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            </div>
+        </>
 
-        </div>
     )
+}
+
+
+{/*<div className="form-floating mb-3">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="email"
+                                        name="email"
+                                        placeholder="name@example.com"
+                                        value={form.email}
+                                        onChange={changeHandler}
+                                    />
+                                    <label htmlFor="email">Email address</label>
+                                </div>*/
+}
+{/*<div className="form-floating">
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="password"
+                                        name="password"
+                                        placeholder="Password"
+                                        value={form.password}
+                                        onChange={changeHandler}
+                                    />
+                                    <label htmlFor="password">Password</label>
+                                </div>*/
+}
+{/*<div className="mt-4">
+                                    <button
+                                        className="btn-dark btn me-3 col-12"
+
+                                        disabled={loading}
+                                        onClick={loginHandler}
+                                    >
+                                        Login
+                                    </button>
+                                    <button
+                                        className={"btn-dark btn me-3 col-12 mt-3"}
+                                        onClick={registerHandler}
+                                        disabled={loading}
+                                    >
+                                        Registration
+                                    </button>
+                                </div>*/
 }
